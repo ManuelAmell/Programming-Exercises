339c6e6ba5f5dd0867e84ec31fe79e69b56a0f5d
ManuelAmell <manuelfcoamell@gmail.com>
2025-06-13 15:43:34 -0500

    ordenamiento de arreglos int y string

--- C++/ej65.cpp
+++ C++/ej65.cpp
@@ -7,8 +7,10 @@ cout <<"ingrese la cantidad de estudiantes :";
 cin >>N;
 cout <<endl<<endl;
 float notas[N];//recordar que sino se declara aqui se guarda basura digital
+string nombres[N];
 for (int x=0;x < N;x++)
-{
+{   cout << "Ingrese el nombre del estudiante : ";
+    fflush(stdin);getline(cin,nombres[x]);
     cout << "ingrese un valor para el estudiante #"<<x+1<<" : ";cin >>notas[x];
     cout << notas[x]<<endl;
 
@@ -31,18 +33,23 @@ cout << "porcentaje de estudiantes con notas mayor al promedio :"<<cantesmayp*10
 for (int actual=0;actual < N-1;actual++)//n -1 por que al finalizar el arreglo estaria en n-2 por que los arreglos comienzan es 0
 {
     int aux;
+    string aux1;
     for (int siguiente = actual + 1 ;siguiente  < N ;siguiente++)
     {  
          if(notas[siguiente] > notas [actual]){
         aux =notas [actual];
         notas [actual]= notas [siguiente];
         notas [siguiente] = aux;
-         }
+        
+        aux1 = nombres[actual];
+        nombres[actual] =nombres[siguiente];
+        nombres[siguiente] =aux1;
+        }
     }
 }
 for (int x=0;x < N;x++)
-{
-    cout <<notas[x]<<" ";
+{cout <<setw(30)<<nombres[x]<<endl;
+    cout <<setw(30) << notas[x]<<endl;
 }
 
     return 0;
