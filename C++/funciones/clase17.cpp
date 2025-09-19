34bd55101e1dc0067ed07e7f94f879bda5bb36a2
Manuel A <manuelfcoamell@gmail.com>
2025-09-19 06:29:07 -0500

    19/9/2025

--- C++/funciones/clase17.cpp
+++ C++/funciones/clase17.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 void menu(int &opc){
     do{
     cout<<"1) cargar arreglo :"<<endl;
@@ -34,6 +35,45 @@ void ImprimirArregloAlrevez(int arr[],int n){
     }
     cout<<endl;
 }
+void BuscarElemento(int arr[],int &n,int &encontrar){
+    bool encontrado=false;
+    for(int i =0; i < n;i++){
+        if (encontrar==arr[i]){
+        encontrado=true;
+            cout << " el numero "<<encontrar<<" fue encontradoe en la posicion "<<i+1<<endl;
+        }
+    }
+    if (!encontrado){ cout << " el numero "<<encontrar<<" no pertenece al arreglo"<<endl;}
+}
+void OrdenarAscendente(int arreglo[],int &n){
+    cout << " arreglo original:"<<endl;
+    for (int i=0;i < n;i++){
+        cout <<"["<<arreglo[i]<<"]  ";
+    }
+    cout << endl;
+     for ( int i=0;i < n-1 ;i++ )
+    {
+        bool orden=false;
+        
+        for ( int x=0;x < n-i-1;x++ )
+        {
+            if (arreglo[x] > arreglo[x+1]) // el signo determina el orden si es mayor o menor
+            {   //swap(arr[x],arr[x+1]);//no valido en clase y mas facil
+                int aux= arreglo[x];
+                arreglo[x]= arreglo[x+1];
+                arreglo[x+1]= aux;
+                orden=true;
+            }
+        }
+        if (!orden){
+                break;
+            }
+    }
+    cout<< " arreglo ordenado :"<<endl;
+    for (int i=0;i < n;i++){
+        cout <<"["<<arreglo[i]<<"]  ";
+    }
+}
 
 
 int main (){
@@ -44,7 +84,6 @@ cin>>n;
 int arreglo[n];
 do{
     menu(opc);
-
 switch (opc)
 {
 case 1:
@@ -52,12 +91,16 @@ case 1:
 
 case 2:ImprimirArreglo(arreglo,n);break;
 
-case 3:ImprimirArregloAlrevez(arreglo,n);break;
-
+case 3:
+ImprimirArregloAlrevez(arreglo,n);break;
 case 4:
+int encontrar;
+cout << "Numero a buscar :";
+cin>> encontrar;
+BuscarElemento(arreglo,n,encontrar);
 break;
-case 5:break;
-
+case 5:OrdenarAscendente(arreglo,n);
+break;
 case 6:
 break;
 case 7:
