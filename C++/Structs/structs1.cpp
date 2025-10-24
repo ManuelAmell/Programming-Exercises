26439807232f1a2b2f9bdfc81b10bac98d3c03ec
Manuel A <manuelfcoamell@gmail.com>
2025-10-24 09:49:34 -0500

    Añadir carpeta Structs

---
+++ C++/Structs/structs1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct infantes {
+    string name;
+    char gener;
+    string especialidad;
+};  
+int main ()
+{
+    
+    return 0;
+}
